12320608a7e69e66eda3ab79ca7cd71167fea2c3
yethon <ursula_yethon@hotmail.com>
2017-08-17 16:37:02 -0400

    Add a button...

--- app/components/eight-ball.js
+++ app/components/eight-ball.js
@@ -1,6 +1,19 @@
 import Component from 'ember-component';
+import get from 'ember-metal/get'
+import set from 'ember-metal/set'
 import injectService from 'ember-service/inject';
 
 export default Component.extend({
-  answers: injectService('answers')
+  answers: injectService('answers'),
+  magicAnswer: '',
+
+  actions: {
+    getAnswer() {
+      let answer = get(this, 'answers').answerMe();
+
+      set(this, 'magicAnswer', answer);
+    }
+  }
+
+
 });
--- tests/integration/components/eight-ball-test.js
+++ tests/integration/components/eight-ball-test.js
@@ -1,3 +1,4 @@
+import { click } from 'ember-native-dom-helpers';
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
@@ -6,12 +7,22 @@ moduleForComponent('eight-ball', 'Integration | Component | eight ball', {
 });
 
 test('it renders', function(assert) {
-
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+  assert.expect(1);
 
   this.render(hbs`{{eight-ball}}`);
 
-  assert.equal(this.$().text().trim(), `HELLO, I'M AN EIGHT BALL.`);
+  assert.equal(this.$('.m8b-answer').text().trim(), ``);
 
+});
+
+test('`shake the magic eight ball - get an answer`', async function(assert) {
+  assert.expect(2);
+
+  this.render(hbs`{{eight-ball}}`);
+
+  assert.ok(this.$('.m8b-answer').text().trim().length === 0, 'there is no message... yet');
+
+  await click('.m8b-answer-button button');
+
+  assert.ok(this.$('.m8b-answer').text().trim().length > 0, 'a message is displayed');
 });
